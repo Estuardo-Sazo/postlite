1825921045d39ab6f452ff81963a7f0119b56ddf
Estuardo-Sazo <estuardo1206@gmail.com>
2021-07-09 06:43:57 -0600

    Modelos y migraciones completo

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'phone',
         'email',
+        'status',
+        'image',
         'password',
+        'profile'
     ];
 
     /**
